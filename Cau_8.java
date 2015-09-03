c2fa4135de281fe7ab3e4c4ec66d4b9a3da3d771
Thync <dung.isec@gmail.com>
2015-09-03 13:16:37 +0700

    Exercise 2

--- Cau_8.java
+++ Cau_8.java
@@ -1,54 +1,21 @@
 /**
  * Created by Thync on 9/1/2015.
  */
-import java.util.Scanner;
 
 public class Cau_8 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Nhap yeu cau: \n\t 1. So nguyen to\n\t 2. So thuan nghich\n\t 3. So co tong chu so chia het cho 10.\n" +
-                         "Lua chon: ");
-        int choose = scan.nextInt();
-        switch (choose){
-                case 1 :
-                    Nt();
-                    break;
-                case 2 :
-                    Tn();
-                    break;
-                case 3:
-                    Ch();
-                    break;
-        }
+        proc();
     }
-    // So nguyen to;
-    public static void Nt(){
-        for(int i = 10000; i < 10000000; i++){
-            if(check(i)){
-                System.out.print(i+" ");
-            }
-        }
-    }
-    public static boolean check(int x){
-        int c =1;
-        for(int i = 2; i <= Math.sqrt(x); i++){
-            if(x%i == 0){
-                c = 0;
-            }
-        }
-        if(c == 1){
-            return true;
-        }else return false;
-    }
-    // So thuan nghich;
-    public static void Tn(){
+    public static void proc(){
         int a,b,c,d,e,f;
-        System.out.println("\nCac so thuan nghich co 5 den 7 chu so la: \n");
+        System.out.println("\nCac so co 5 den 7 chu so thoa man la: \n");
         for(int i = 10000; i<100000; i++){
             a = i/1000;
             b = i%100;
             if(a == b/10 + (b%10)*10){
-                System.out.print(i+" ");
+                if(Ch(i) && Nt(i)){
+                    System.out.print(i+" ");
+                }
             }
         }
         System.out.println();
@@ -56,7 +23,9 @@ public class Cau_8 {
             c = j/1000;
             d = j%1000;
             if(c == d/100 + ((d/10)%10)*10 + (d%10)*100){
-                System.out.print(j+" ");
+                if(Ch(j) && Nt(j)){
+                    System.out.print(j+" ");
+                }
             }
         }
         System.out.println();
@@ -64,25 +33,32 @@ public class Cau_8 {
             e = k/10000;
             f = k%1000;
             if(e == f/100 + ((f/10)%10)*10 + (f%10)*100){
-                System.out.print(k+" ");
+                if(Ch(k) && Nt(k)){
+                    System.out.print(k+" ");
+                }
             }
         }
     }
     // So co tong chu so chia het cho 10;
-    public static void Ch(){
-        for(int i = 10000; i < 10000000; i++){
-            if(chiahet(i)){
-                System.out.print(i+" ");
-            }
-        }
-    }
-    public static boolean chiahet(int x){
+    public static boolean Ch(int x){
         int sum = 0;
         while(x > 0){
             sum = sum + x%10;
             x = x/10;
         }
         if(sum % 10 == 0){
+            return true;
+        }else return false;
+    }
+    // So nguyen to;
+    public static boolean Nt(int x){
+        int c =1;
+        for(int i = 2; i <= Math.sqrt(x); i++){
+            if(x%i == 0){
+                c = 0;
+            }
+        }
+        if(c == 1){
             return true;
         }else return false;
     }
--- Cau_9.java
+++ Cau_9.java
@@ -1,43 +1,35 @@
 /**
  * Created by Thync on 9/2/2015.
  */
-import java.util.Scanner;
-
 public class Cau_9 {
     public static int [] arr = new int[8];
     public static int [] boso = {2, 3, 5, 7};
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Nhap yeu cau so co 7 chu so la: " +
-                "\n\t 1. So nguyen to" +
-                "\n\t 2. Tat ca cac chu so la so nguyen to" +
-                "\n\t 3. Dao cua no cung la so nguyen to\n" +
-                "Lua chon: ");
-        int choose = scan.nextInt();
-        switch (choose){
-            case 1 :
-                NgTo();
-                break;
-            case 2 :
-                CsNt();
-                break;
-            case 3:
-                Dao();
-                break;
+        Try(0);
+    }
+    // So co tat ca cac chu so la so nguyen to;
+    public static void Try(int i){
+        for(int j = 0; j < 4; j++){
+            arr[i] = boso[j];
+            if(i == 6){
+                proc();
+            } else Try(i + 1);
+        }
+    }
+    public static void proc(){
+        int temp = 0;
+        for(int i = 0; i < 7; i++){
+            temp = temp*10 + arr[i];
+        }
+        if(Nt(temp) && Nt(Reserve(temp))) {
+            System.out.println(temp);
         }
     }
     // So nguyen to;
-    public static void NgTo(){
-        for(int i = 1000000; i < 10000000; i++){
-            if(Nt(i)){
-                System.out.println(i);
-            }
-        }
-    }
-    public static boolean Nt(int x){
+    public static boolean Nt(int n){
         int check = 1;
-        for(int i = 2; i <= Math.sqrt(x); i++){
-            if(x % i == 0){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
                 check = 0;
             }
         }
@@ -45,34 +37,15 @@ public class Cau_9 {
             return true;
         } else return false;
     }
-    // So co tat ca cac chu so la so nguyen to;
-    public static void CsNt(){
-        Try(0);
-    }
-    public static void Try(int i){
-        for(int j = 0; j < 4; j++){
-            arr[i] = boso[j];
-            if(i == 6){
-                print();
-            } else Try(i + 1);
-        }
-    }
-    public static void print(){
-        for(int i = 0; i < 7; i++){
-            System.out.print(arr[i]);
-        }
-        System.out.println();
-    }
     // So ma dao cua no cung la so nguyen to;
-    public static void Dao(){
-        int temp = 0;
-        for(int i = 1000000; i < 10000000; i++){
-            if(Nt(i)){
-                temp = (i/1000000 + ((i/100000)%10)*10 + ((i/10000)%10)*100 + ((i/1000)%10)*1000 + ((i/100)%10)*10000 + ((i/10)%10)*100000 + (i%10)*1000000);
-                if(Nt(temp)){
-                    System.out.println(i);
-                }
-            }
-        }
+    public static int Reserve(int n){
+        int rev = n/1000000
+                + ((n/100000)%10)*10
+                + ((n/10000)%10)*100
+                + ((n/1000)%10)*1000
+                + ((n/100)%10)*10000
+                + ((n/10)%10)*100000
+                + (n%10)*1000000;
+        return rev;
     }
 }
